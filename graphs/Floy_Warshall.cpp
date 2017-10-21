2ecee468391546cbcf292a0700a3694828b903d9
benni515 <bennilinn@live.com>
2017-10-21 00:23:33 +0000

    Added floyd warshall algorithm

---
+++ graphs/Floy_Warshall.cpp
@@ -0,0 +1,7 @@
+void floyd_warshall(int** arr, int n) {
+	rep(k,0,n) rep(i,0,n) rep(j,0,n) {
+		if(arr[i][k] != INF && arr[k][j] != INF) {
+			arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]); 
+		}
+	}
+}
