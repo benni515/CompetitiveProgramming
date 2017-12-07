7e3b7e8b17e8d71e97692e459ce930121842a972
benni515 <bennilinn@live.com>
2017-12-07 09:20:21 +0000

    Changed Header, added Cycle_Check, which checks if there is a cycle in a graph, even if it's directed

--- header.cpp
+++ header.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+template <class T> int size(const T &x) { return x.size(); }
 #define rep(i,a,b) for(int i = (a); i < (b); i++)
 #define iter(it,c) for(auto it = (c).begin(); it != (c).end();++it)
 #define pb push_back
@@ -9,12 +10,12 @@ typedef long long ll;
 typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
+
 const int INF = ~(1<<31);
 const double pi = acos(-1);
+const double EPS = 1e-9;
 
 int main() {
     cin.sync_with_stdio(false);
     return 0;
 }
-
-
