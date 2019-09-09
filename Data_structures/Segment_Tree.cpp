78ca801be59fd000950489145c98f56c89c1d068
bernhard Linn Hilmarsson <bennilinn@gmail.com>
2019-09-09 11:03:36 +0000

    updated segment tree, old implementation slow

--- Data_structures/Segment_Tree.cpp
+++ Data_structures/Segment_Tree.cpp
@@ -1,59 +1,41 @@
 struct node {
-	node* left = NULL;
-	node* right = NULL;
-	int sum, mx, mn, atr, atl;
-	node(node* l, node* r, int v, int al, int ar) {
-		left = l; right = r;
-		sum = v; mx = v; mn = v;
-		atl = al; atr = ar;
-	}
-	void change(int at, int to) { // changes index of at into value of to
-		if(at > atr || at < atl) return;
-		if(at == atr && at == atl) {
-			sum = to; mx = to; mn = to;
-			return;
-		}
-		left->change(at,to);
-		right->change(at,to);
-		update();
-	}
-	void add(int at, int by) { // adds by to the index of at
-		if(at > atr || at < atl) return;
-		if(at == atr && at == atl) {
-			sum+=by;mx+=by;mn+=by;
-			return;
-		}
-		left->add(at,by);
-		right->add(at,by);
-		update();
-	}
-	void update() { // Changes everything when done
-		sum = left->sum+right->sum;
-		mn = min(left->mn, right->mn);
-		mx = max(left->mx, right->mx);
-	}
-	int qsum(int l, int r) { // gets the some from range l to r
-		if(l > atr || r < atl) return 0;
-		if(l <= atl && atr <= r) return sum;
-		return left->qsum(l,r)+right->qsum(l,r);
-	}
-	int qmn(int l, int r) { // gets the min value from l to r
-		if(l > atr || r < atl) return INF;
-		if(l <= atl && atr <= r) return mn;
-		return min(left->qmn(l,r),right->qmn(l,r));
-	}
-	int qmx(int l, int r) { // gets the max value from l to r
-		if(l > atr || r < atl) return 0;
-		if(l <= atl && atr <= r) return mx;
-		return max(left->qmx(l,r), right->qmx(l,r));
-	}
+    int val;
+    node(int _val) { val = _val; };
 };
-node* build(int l, int r) {
-	if(l > r) return NULL;
-	if(l == r) return new node(NULL,NULL,0,l,r);
-	int m = (l+r)/2;
-	node* left = build(l,m);
-	node* right = build(m+1,r);
-	node* cur = new node(left,right,0,l,r);
-	return cur;
-}
+struct segment_tree {
+    int sz, n;
+    vector<node> t;
+    segment_tree(int _n) : sz(_n*4), n(_n) {
+        rep(i,0,sz) t.pb(node(0));
+    }
+    void build(vi &a, int v=1, int tl=0, int tr=-1) {
+        if(tr == -1) tr = n-1;
+        if(tl == tr) {
+            t[v].val = a[tl];
+        } else {
+            int tm = (tl+tr)/2;
+            build(a, v*2, tl, tm);
+            build(a, v*2+1, tm+1, tr);
+            t[v] = node(t[v*2].val+t[v*2+1].val);
+        }
+    }
+    node query(int l, int r, int v=1, int tl=0, int tr=-1) {
+        if(tr == -1) tr = n-1;
+        if(l > r) return node(0);
+        if(l == tl && r == tr) return t[v];
+        int tm = (tl+tr)/2;
+        return node( query(l,min(r,tm), v*2, tl, tm).val
+                + query(max(l,tm+1), r, v*2+1, tm+1, tr).val);
+    
+    }
+    void update(int pos, int new_val, int v=1, int tl=0, int tr=-1) {
+        if(tr == -1) tr = n-1;
+        if(tl == tr) t[v].val = new_val;
+        else {
+            int tm = (tl+tr)/2;
+            if(pos <= tm) update(pos, new_val, v*2, tl, tm);
+            else update(pos, new_val, v*2+1, tm+1, tr);
+            t[v].val = t[v*2].val + t[v*2+1].val;
+        }
+    }
+};
