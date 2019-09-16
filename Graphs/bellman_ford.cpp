88884eb9c1431b69700bf03537c34bfa6b65fa6e
bernhard Linn Hilmarsson <bennilinn@gmail.com>
2019-09-16 11:02:42 +0000

    bellman ford was wrong, need to fix more

--- Graphs/bellman_ford.cpp
+++ Graphs/bellman_ford.cpp
@@ -15,14 +15,16 @@ vi bellman_ford(vector<pair<ii,int>> &edges, int n, int u) {
         }
         if(!changed) break;
     }
-    rep(j,0,size(edges)) {
-        if(D[edges[j].fs.fs] != INF) {
-            int new_val = 0;
-            if(D[edges[j].fs.fs] == -INF) new_val = -INF;
-            else new_val = D[edges[j].fs.fs]+edges[j].sc;
-            if(new_val < D[edges[j].fs.sc]) {
-                D[edges[j].fs.sc] = -INF;
-                D[edges[j].fs.fs] = -INF;
+    rep(i,0,n) {
+        rep(j,0,size(edges)) {
+            if(D[edges[j].fs.fs] != INF) {
+                int new_val = 0;
+                if(D[edges[j].fs.fs] == -INF) new_val = -INF;
+                else new_val = D[edges[j].fs.fs]+edges[j].sc;
+                if(new_val < D[edges[j].fs.sc]) {
+                    D[edges[j].fs.sc] = -INF;
+                    D[edges[j].fs.fs] = -INF;
+                }
             }
         }
     }
