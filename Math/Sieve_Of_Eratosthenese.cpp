b65bc0c020b3b7f2479488a2a8253d1295d6c015
bernhard Linn Hilmarsson <bennilinn@gmail.com>
2019-09-16 10:47:19 +0000

    fixed prime sieve

--- Math/Sieve_Of_Eratosthenese.cpp
+++ Math/Sieve_Of_Eratosthenese.cpp
@@ -1,13 +1,10 @@
-vi Sieve(int n){
-	bitset<100000901> prime;
-	prime.set(1);
-	vi primes;
-	for(int p = 2; p<=n;p++) {
-		if(!prime.test(p)) { // check if bit is set
-			primes.push_back(p);
-			// set all bits that are a multiple of the prime number
-			for(int i = p*2; i<=n;i+=p) prime.set(i); 
-		} 
-	}
-	return primes;
+void Sieve(int n){
+    bitset<100000020> prime;
+    prime.flip();
+    prime.flip(1);
+    for(int p = 2; p*p<=n;p++) {
+        if(prime.test(p)) {
+            for(int i = p*p; i<=n;i+=p) prime.set(i,0);
+        }
+    }
 }
