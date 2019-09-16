74e7c02ec2bc49c052583c1dc5cc75374d123b22
bernhard Linn Hilmarsson <bennilinn@gmail.com>
2019-09-16 10:30:23 +0000

    Added bellman ford implementation

---
+++ Graphs/bellman_ford.cpp
@@ -0,0 +1,30 @@
+vi bellman_ford(vector<pair<ii,int>> &edges, int n, int u) {
+    vi D(n);
+    rep(i,0,n) D[i] = INF;
+    D[u] = 0;
+    rep(i,0,n) {
+        bool changed = false;
+        rep(j,0,size(edges)) {
+            if(D[edges[j].fs.fs] != INF) {
+                int new_val = D[edges[j].fs.fs]+edges[j].sc;
+                if(new_val < D[edges[j].fs.sc]) {
+                    changed = true;
+                    D[edges[j].fs.sc] = new_val;
+                }
+            }
+        }
+        if(!changed) break;
+    }
+    rep(j,0,size(edges)) {
+        if(D[edges[j].fs.fs] != INF) {
+            int new_val = 0;
+            if(D[edges[j].fs.fs] == -INF) new_val = -INF;
+            else new_val = D[edges[j].fs.fs]+edges[j].sc;
+            if(new_val < D[edges[j].fs.sc]) {
+                D[edges[j].fs.sc] = -INF;
+                D[edges[j].fs.fs] = -INF;
+            }
+        }
+    }
+    return D;
+}
